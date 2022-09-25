8c13f00bbb968b5556f83de20b0b266e1b61b099
nidhi <nidhi@LAPTOP-OVSP9O9O>
2022-09-25 19:58:03 +0530

    asdfhh

--- HelloWorld/src/main/java/MyFirst.java
+++ HelloWorld/src/main/java/MyFirst.java
@@ -3,7 +3,7 @@ public class MyFirst {
 	public String MyName(String name1) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello, World"+ name1 ); 
-		return name;
+		return name1;
 
 	}
 
